612b7b7ab13d551bcaad74d7c9f5ffac8a2f9544
tunkhine126 <tunkhine126@gmail.com>
2019-07-22 16:30:01 -0400

    Controller updates

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render json: @user
+    render json: UserSerializer.new(@user)
   end
   
   def destroy
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,6 +25,7 @@ class CommentsController < ApplicationController
 
   def show
     @comment = Comment.find(params[:id])
+    render json: ShowSerializer.new(@show)
   end
   
   private
