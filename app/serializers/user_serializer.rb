9be6ce912bc9aec73aa763f7c63cc87c220108a5
tunkhine126 <tunkhine126@gmail.com>
2019-07-29 22:49:52 -0400

    Adjusting Controllers and Serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :location, :id, :comments, :rides, :likes, :followed_users, :follower_users, :followers, :img_url, :bikes
+  attributes :username, :location, :email, :id, :comments, :rides, :likes, :followed_users, :follower_users, :followers, :img_url, :bikes
 end
