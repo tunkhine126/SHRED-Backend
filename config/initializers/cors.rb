6710cdcc0259d3ab881b8cff604ce2ff153a437c
tunkhine126 <tunkhine126@gmail.com>
2019-08-08 14:54:44 -0400

    CORS update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://shred-frontend.firebaseapp.com'
 
     resource '*',
       headers: :any,
